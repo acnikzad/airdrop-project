b04941334c159068219bfc4d3a5bb0df5021fcbd
Aria Nikzad <a.c.nikzad@gmail.com>
2022-09-23 18:27:12 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+// console.log("hello")
+const {
+    Connection,
+    PublicKey,
+    clusterApiUrl,
+    Keypair,
+    LAMPORTS_PER_SOL
+} = require("@solana/web3.js")
+
+const wallet = new Keypair()
+
+const publicKey = wallet._keypair.publicKey
+const secretKey = wallet._keypair.secretKey
+
+console.log(publicKey)
+console.log(secretKey)
